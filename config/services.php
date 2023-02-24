a31ef1ab1eccbbad4cd4d0619bac91403deff7ff
Dhimas Wahyu Prayogi <dhimaswahyu0846@gmail.com>
2023-02-24 16:25:40 +0700

    Laravel Jetstream

---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'google' => [
+      'client_id' => '1059915945479-imve34f7r9tlqkarv22h497tiiuravu6.apps.googleusercontent.com',
+      'client_secret' => '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',
+      'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+    ]
+];
